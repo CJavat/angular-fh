170d164c6a07083ecfbb82907575cf54aa103adf
CJavatX <cdpm98@hotmail.com>
2024-08-07 17:52:29 -0600

    SECCIÓN 9 / CAP 11 - EN CURSO

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule, CountriesModule],
   providers: [],
   bootstrap: [AppComponent],
 })

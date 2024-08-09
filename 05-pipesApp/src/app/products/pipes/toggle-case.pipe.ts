31b99ef5204d76da7ec98b6b25f05d370af2c420
CJavatX <cdpm98@hotmail.com>
2024-08-09 16:39:59 -0600

    SECCIÓN 12 - TERMINADO

---
+++ 05-pipesApp/src/app/products/pipes/toggle-case.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+// daniel | toggleCase = 'DANIEL'
+// DANIEL | toggleCase = 'daniel'
+
+@Pipe({
+  name: 'toggleCase',
+})
+export class ToggleCasePipe implements PipeTransform {
+  constructor() {}
+
+  transform(value: string, toUpper: boolean = false): string {
+    return toUpper ? value.toUpperCase() : value.toLowerCase();
+  }
+}

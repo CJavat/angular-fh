2ed1515e6d07f9c809aea5ee213f9b646b1ac335
CJavatX <cdpm98@hotmail.com>
2024-08-12 13:19:30 -0600

    SECCIÓN 15 - TERMINADO

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environments';
 
@@ -22,5 +22,24 @@ export class HeroesService {
 
   public getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes?q=${query}&_limit=6`);
+  }
+
+  public addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseURL}/heroes`, hero);
+  }
+
+  public updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error(`Hero id is required`);
+
+    return this.http.patch<Hero>(`${this.baseURL}/heroes/${hero.id}`, hero);
+  }
+
+  public deleteHeroById(id: string): Observable<boolean> {
+    if (!id) throw new Error(`Hero id is required`);
+
+    return this.http.delete(`${this.baseURL}/heroes/${id}`).pipe(
+      map((resp) => true) //! Si no hay error, devuelve TRUE.
+      catchError((err) => of(false)), //! Si hay un error, devuelve FALSE.
+    );
   }
 }

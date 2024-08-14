b1979d6efc44e92a874ba129b9bc5283dc023006
CJavatX <cdpm98@hotmail.com>
2024-08-14 11:36:00 -0600

    SECCIÓN 18 - TERMINADO

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,8 +1,64 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/services/email-validator.service';
+// import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   selector: 'auth-register-page',
   templateUrl: './register-page.component.html',
   styles: ``,
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ) {
+    this.myForm = this.formbuilder.group(
+      {
+        name: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              this.validatorsService.firstNameAndLastnamePattern
+            ),
+          ],
+        ],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(this.validatorsService.emailPattern),
+          ],
+          [this.emailValidator], //? [new EmailValidatorService()], -> Otra forma de hacerlo sin la inyección de dependencias.
+        ],
+        username: [
+          '',
+          [Validators.required, this.validatorsService.cantBeStrider],
+        ],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        password2: ['', Validators.required],
+      },
+      {
+        validators: [
+          this.validatorsService.isFieldOneEqualFieldTWo(
+            'password',
+            'password2'
+          ),
+        ],
+      }
+    );
+  }
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
--- 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-switches-page',
   templateUrl: './switches-page.component.html',
   styles: ``,
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup;
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  constructor(private formbuilder: FormBuilder) {
+    this.myForm = this.formbuilder.group({
+      gender: ['M', Validators.required],
+      wantNotifications: [true, Validators.required],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+  }
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  onSave() {
+    if (this.myForm.invalid) return this.myForm.markAllAsTouched();
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+}

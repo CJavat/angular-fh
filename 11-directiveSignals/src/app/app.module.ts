7396b756227a8158cf45d5102d5f1efa6e9f258b
CJavatX <cdpm98@hotmail.com>
2024-08-16 12:46:11 -0600

    SECCIÓN 24 - TERMINADO

--- 11-directiveSignals/src/app/app.module.ts
+++ 11-directiveSignals/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ProductsModule } from './products/products.module';
-import { SharedModule } from './shared/shared.module';
 import { SignalsModule } from './signals/signals.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ProductsModule, SignalsModule],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, OnDestroy, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy {
+  public user = signal<User>({
+    id: 0,
+    email: '',
+    first_name: '',
+    last_name: '',
+    avatar: '',
+  });
+  public fullName = computed<string>(
+    () => `${this.user()!.first_name} ${this.user()!.last_name}`
+  );
 
+  public userChangedEffect = effect(() => {
+    console.log(this.user().first_name);
+  });
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+
+  onFieldUpdated(field: string, value: string) {
+    // this.user.update((current) => ({
+    //   ...current,
+    //   [field]: value,
+    // }));
+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'id':
+          current.id = Number(value);
+          break;
+
+        default:
+          return current;
+      }
+
+      return current;
+    });
+  }
 }
